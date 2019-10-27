5523fb9fe40387a9e25559f572a9e3bbbf67cf1c
Saara Koskipaa <saara.koskipaa@iki.fi>
2019-10-27 20:46:40 +0200

    puh.luettelon backend 3.7, 3.8*

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 app.use(bodyParser.json())
+//app.use(morgan('tiny'))
+
+morgan.token('body', function (request, response) {
+    return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 
 let persons = [
     {
