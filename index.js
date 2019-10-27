6c6039335e31477e47d5089abdaf1af79d1f3eb7
Saara Koskipaa <39309728+skoskipaa@users.noreply.github.com>
2019-10-27 12:16:32 +0200

    puh.luettelon backend 3.1 - 3.6

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        name: "John Doe",
+        number: "555-123456",
+        id: 1
+    },
+    {
+        name: "Jane Doe",
+        number: "555-654321",
+        id: 2
+    },
+    {
+        name: "Bond, James Bond",
+        number: "007",
+        id: 3
+    }
+]
+app.get('/info', (request, response) => {
+    const users = persons.length
+    const now = new Date()
+    response.send(`<span>Phonebook has ${users} contacts</span>
+                   <br>
+                   <span>${now}</span>`)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(pers => pers.id === id)
+   
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(pers => pers.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    if (persons.map(pers => pers.name).includes(body.name)) {
+        return response.status(400).json({
+            error: 'name is already in the phonebook'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const generateId = () => {
+    const newId = Math.floor(Math.random() * Math.floor(1000))
+
+    return newId
+}
+
+
+const port = 3001
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
